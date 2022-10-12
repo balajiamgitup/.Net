0a53f7f08155e4990d8e3d631f4ca6b115f96d49
Balaji A M <104089954+balajiamgitup@users.noreply.github.com>
2022-10-12 17:43:37 +0530

    Add files via upload

---
+++ Programs/hashtable_array.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Hashtable ht = new Hashtable();
+            ht.Add("Name","Balaji");
+            ht.Add("age","23");
+            ht.Add("salary", "27000");
+            ht.Add("account", "TSG");
+            DictionaryEntry[] myarr = new DictionaryEntry[ht.Count];
+            ht.CopyTo(myarr, 0);
+            for (int i = 0; i < myarr.Length; i++)
+                Console.WriteLine(myarr[i].Key + " --> "
+                                  + myarr[i].Value);
+
+        }
+    }
+}
